72418b240353b0e042a08917a6ccde2064fd37e1
Mudit <muditparashar@2410@gmail.com>
2023-11-16 00:21:05 +0530

    basic unresponsive working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.css'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Dashboard from './components/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>
 );
 
